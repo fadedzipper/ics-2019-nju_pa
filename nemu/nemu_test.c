7e2f804e666a7d5717b7f88a122965dde9d920a6
tracer-ics2019 <tracer@njuics.org>
2020-05-26 10:31:52 +0800

    >  compile 0905170211 mff Linux mff-Inspiron-5567 4.15.0-102-generic #103-Ubuntu SMP Fri May 15 15:22:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:31:52 up  2:23,  4 users,  load average: 0.22, 0.16, 0.07 fc9582b63e27f33e90b50dc093b260a6535ae8b5

---
+++ nemu/nemu_test.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdio.h>
+typedef struct {
+  struct {
+	union{
+		uint32_t _32;
+		uint16_t _16;
+		uint8_t _8[2];
+	};
+
+  } gpr[8];
+
+  /* Do NOT change the order of the GPRs' definitions. */
+
+  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+   * in PA2 able to directly access these registers.
+   */
+
+
+} CPU_state;
+int main(){
+	CPU_state cpu;
+	cpu.gpr[0]._32 = 0x87654321;
+	printf("%x\n", cpu.gpr[0]._16);
+	printf("%x\n", cpu.gpr[0]._8[0]);
+	printf("%x\n", cpu.gpr[0]._8[1]);
+	return 0;
+}
