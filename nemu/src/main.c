59d79c8507189f0af8b38ad4f5d0430c194b5eb2
tracer-ics2019 <tracer@njuics.org>
2020-06-05 22:44:07 +0800

    >  compile 0905170211 mff Linux mff-Inspiron-5567 4.15.0-102-generic #103-Ubuntu SMP Fri May 15 15:22:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:44:07 up  7:33,  4 users,  load average: 1.14, 1.16, 1.21 e915efb6d629406c942763f24e4f6838be39c46b

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,12 +1,33 @@
+#include <stdio.h>
+#include "nemu.h"
+#include <stddef.h>
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+uint32_t expr(char *e, bool *success);
+
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+//  int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
+ // ui_mainloop(is_batch_mode);
+	bool success = true;
+	FILE *fp = NULL;
+	fp = fopen("../tools/gen-expr/input", "r");
+	assert(fp != NULL);
+	unsigned int result = 0;
+	char inputexprbuf[65536];
+	int ret = 0;
+	int line = 0;
+	while((ret = fscanf(fp, "%u %s", &result, inputexprbuf) != EOF)){
+		if(result == expr(inputexprbuf, &success)){
+			printf("success\n");
+		}
+		else{
+			printf("failed in %d line\n", ++line);
+		}
+	}
 
   return 0;
 }
