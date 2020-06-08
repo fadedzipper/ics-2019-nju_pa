a78012f5de8215fa65261ea105cba5dd3ff8fe39
tracer-ics2019 <tracer@njuics.org>
2020-06-08 20:21:13 +0800

    >  compile 0905170211 mff Linux mff-Inspiron-5567 4.15.0-102-generic #103-Ubuntu SMP Fri May 15 15:22:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:21:13 up  4:41,  3 users,  load average: 0.23, 0.27, 0.21 c3d1b2ff9513a83329010a9f553fbed9964c085f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,6 +7,7 @@
 #include <regex.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum {
     TK_NOTYPE = 256,
@@ -67,16 +68,14 @@ void init_regex()
 
 typedef struct token {
     int type;
-    char str[32];
+    char str[35];
 } Token;
 
-static Token tokens[65536] __attribute__ ((used)) = {
-};
+static Token tokens[65536] __attribute__ ((used)) = {};
 
 static int nr_token __attribute__ ((used)) = 0;
 
-static bool make_token(char *e)
-{
+static bool make_token(char *e){
     int position = 0;
     int i;
     regmatch_t pmatch;
@@ -85,44 +84,42 @@ static bool make_token(char *e)
 
     while (e[position] != '\0') {
 	/* Try all rules one by one. */
-	for (i = 0; i < NR_REGEX; i++) {
-	    if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0
-		&& pmatch.rm_so == 0) {
-		char *substr_start = e + position;
-		int substr_len = pmatch.rm_eo;
-
-		Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
-		position += substr_len;
-
-		/* TODO: Now a new token is recognized with rules[i]. Add codes
-		 * to record the token in the array `tokens'. For certain types
-		 * of tokens, some extra actions should be performed.
-		 */
-		switch (rules[i].token_type) {
-		case TK_INT:
-		    tokens[nr_token].type = TK_INT;
-		    int j;
-		    for (j = 0; j < substr_len && j < 31; j++) {
-			tokens[nr_token].str[j] = substr_start[j];
+		for (i = 0; i < NR_REGEX; i++) {
+		    if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0
+				&& pmatch.rm_so == 0) {
+				char *substr_start = e + position;
+				int substr_len = pmatch.rm_eo;
+	
+				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				position += substr_len;
+	
+				/* TODO: Now a new token is recognized with rules[i]. Add codes
+				 * to record the token in the array `tokens'. For certain types
+				 * of tokens, some extra actions should be performed.
+				 */
+				switch (rules[i].token_type) {
+				case TK_INT:
+				    tokens[nr_token].type = TK_INT;
+					assert(substr_len < 33);
+					strncpy(tokens[nr_token].str, substr_start, substr_len);
+					tokens[nr_token].str[substr_len] = '\0';
+				    break;
+	
+				default:
+				    tokens[nr_token].type = rules[i].token_type;
+				    break;
+				}
+				nr_token++;
+	
+				break;
 		    }
-		    tokens[nr_token].str[j] = '\0';
-		    break;
-
-		default:
-		    tokens[nr_token].type = rules[i].token_type;
-		    break;
 		}
-
-		break;
-	    }
-	}
-
-	if (i == NR_REGEX) {
-	    printf("no match at position %d\n%s\n%*.s^\n", position, e,
-		   position, "");
-	    return false;
-	}
-	nr_token++;
+	
+		if (i == NR_REGEX) {
+		    printf("no match at position %d\n%s\n%*.s^\n", position, e,
+			   position, "");
+		    return false;
+		}
     }
 
     return true;
@@ -133,39 +130,40 @@ bool check_parentheses(int p, int q, bool * success)
     int flag = 0;
     int i;
     for (i = p; i <= q; i++) {
-	if (tokens[i].type == TK_LP) {
-	    flag++;
-	}
-	if (tokens[i].type == TK_RP) {
-	    flag--;
-	}
-	if (flag < 0) {
-	    *success = false;
-	    return false;
-	}
+		if (tokens[i].type == TK_LP) {
+		    flag++;
+		}
+		if (tokens[i].type == TK_RP) {
+		    flag--;
+		}
+		if (flag < 0) {
+		    *success = false;
+		    return false;
+		}
     }
     if (flag != 0) {
-	*success = false;
-	return false;
+		*success = false;
+		return false;
     }
     flag = 0;
     bool istrue = true;
     for (i = p; i <= q; i++) {
-	if (tokens[i].type == TK_LP) {
-	    flag++;
-	}
-	if (tokens[i].type == TK_RP) {
-	    flag--;
-	}
-	if (i != q && flag == 0) {
-	    istrue = false;
-	    break;
-	}
+		if (tokens[i].type == TK_LP) {
+		    flag++;
+		}
+		if (tokens[i].type == TK_RP) {
+		    flag--;
+		}
+		if (i != q && flag == 0) {	//if contains '(' and ')' in p and q perspectively ,flag > 0
+		    istrue = false;
+		    break;
+		}
     }
     if (istrue == true) {
-	return true;
-    } else {
-	return false;
+		return true;
+    }
+	else {
+		return false;
     }
 }
 
@@ -175,22 +173,22 @@ int find_maincalsymbol(int p, int q)
     int highlevelpos = -1;	//high priority symbol
     int i, inLP = 0;
     for (i = p; i <= q; i++) {
-	if (tokens[i].type == TK_LP) {
-	    inLP++;
-	}
-	if (tokens[i].type == TK_RP) {
-	    inLP--;
-	}
-	if (inLP > 0) {
-	    continue;
-	}
-
-	if (tokens[i].type == TK_ADD || tokens[i].type == TK_MINUS) {
-	    lowlevelpos = i;
-	}
-	if (tokens[i].type == TK_STAR || tokens[i].type == TK_DIV) {
-	    highlevelpos = i;
-	}
+		if (tokens[i].type == TK_LP) {
+		    inLP++;
+		}
+		if (tokens[i].type == TK_RP) {
+		    inLP--;
+		}
+		if (inLP > 0) {
+		    continue;
+		}
+	
+		if (tokens[i].type == TK_ADD || tokens[i].type == TK_MINUS) {
+		    lowlevelpos = i;
+		}
+		if (tokens[i].type == TK_STAR || tokens[i].type == TK_DIV) {
+		    highlevelpos = i;
+		}
     }
     assert(lowlevelpos == -1 && highlevelpos == -1);	//if this failed ,indicate check_pathese... is wrong. 
     return lowlevelpos == -1 ? highlevelpos : lowlevelpos;
@@ -199,66 +197,91 @@ int find_maincalsymbol(int p, int q)
 uint32_t eval(int p, int q, bool * success)
 {
     if (p > q) {
-	/* Bad expression */
-	*success = false;
-	return 0;
-    } else if (p == q) {
-	/* Single token.
-	 * For now this token should be a number.
-	 * Return the value of the number.
-	 */
-	if (tokens[p].type != TK_INT) {
-	    *success = false;
-	    return 0;
-	}
-	uint32_t val = (uint32_t) atoi(tokens[p].str);
-	return val;
-    } else if (check_parentheses(p, q, success) == true) {
-	/* The expression is surrounded by a matched pair of parentheses.
-	 * If that is the case, just throw away the parentheses.
-	 */
-	return eval(p + 1, q - 1, success);
-    } else {
-	if (*success == false) {
-	    return 0;
-	}
-	int op = find_maincalsymbol(p, q);
-	uint32_t val1 = eval(p, op - 1, success);
-	if (*success == false) {
-	    return 0;
-	}
-	uint32_t val2 = eval(op + 1, q, success);
-	if (*success == false) {
-	    return 0;
-	}
-
-	switch (tokens[op].type) {
-	case TK_ADD:
-	    return val1 + val2;
-	case TK_MINUS:
-	    return val1 - val2;
-	case TK_STAR:
-	    return val1 * val2;
-	case TK_DIV:
-	    return val1 / val2;
-	default:
-	    assert(0);
-	}
+		/* Bad expression */
+		*success = false;
+		return 0;
+    }
+	else if (p == q) {
+		/* Single token.
+		 * For now this token should be a number.
+		 * Return the value of the number.
+		 */
+		if (tokens[p].type != TK_INT) {
+		    *success = false;
+		    return 0;
+		}
+		uint32_t val = (uint32_t) atoi(tokens[p].str);
+		return val;
+    } 
+	else if (check_parentheses(p, q, success) == true) {
+		/* The expression is surrounded by a matched pair of parentheses.
+		 * If that is the case, just throw away the parentheses.
+		 */
+		return eval(p + 1, q - 1, success);
+    }
+	else {
+		if (*success == false) {
+		    return 0;
+		}
+		int op = find_maincalsymbol(p, q);
+		uint32_t val1 = eval(p, op - 1, success);
+		if (*success == false) {
+		    return 0;
+		}
+		uint32_t val2 = eval(op + 1, q, success);
+		if (*success == false) {
+		    return 0;
+		}
+	
+		switch (tokens[op].type) {
+		case TK_ADD:
+		    return val1 + val2;
+		case TK_MINUS:
+		    return val1 - val2;
+		case TK_STAR:
+		    return val1 * val2;
+		case TK_DIV:
+		    return val1 / val2;
+		default:
+		    assert(0);
+		}
     }
 }
 
 uint32_t expr(char *e, bool * success)
 {
     if (!make_token(e)) {
-	*success = false;
-	return 0;
+		*success = false;
+		return 0;
     }
 
     /* TODO: Insert codes to evaluate the expression. */
     uint32_t val = eval(0, nr_token - 1, success);
     if (*success == true) {
-	return val;
+		return val;
     }
 
     return 0;
 }
+
+//int main(void){
+///* 例如在check_parentheses()函数中, (4 + 3)) * ((2 - 1)和(4 + 3) * (2 - 1)这两个表达式虽然都返回false, 因为前一种情况是表达式不合法, 是没有办法成功进行求值的; 而后一种情况是一个合法的表达式, 是可以成功求值的,*/ 
+///*	"(2 - 1)"             // true
+//	"(4 + 3 * (2 - 1))"   // true
+//	"4 + 3 * (2 - 1)"     // false, the whole expression is not surrounded by a matched
+//	                      // pair of parentheses
+//	"(4 + 3)) * ((2 - 1)" // false, bad expression
+//	"(4 + 3) * (2 - 1)"   // false, the leftmost '(' and the rightmost ')' are not matched */
+//	tokens[0].type = TK_LP;
+//	tokens[1].type = TK_INT;
+//	tokens[2].type = TK_MINUS;
+//	tokens[3].type = TK_INT;
+//	tokens[4].type = TK_RP;
+//	
+//	strcat(tokens[1].str, "2");
+//	strcat(tokens[3].str, "1");
+//
+//	bool success = true;
+//	printf("%u", eval(0, 4, success));
+//	return 0;
+//}
