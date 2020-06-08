e71daf4b879216936a2c30a45d9b8c544ef08017
tracer-ics2019 <tracer@njuics.org>
2020-06-08 20:50:23 +0800

    >  compile 0905170211 mff Linux mff-Inspiron-5567 4.15.0-102-generic #103-Ubuntu SMP Fri May 15 15:22:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:50:23 up  5:10,  4 users,  load average: 0.08, 0.11, 0.14 a7ee3a1a9e83cd36f9c23c691fd2404ddb858a93

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -8,14 +8,14 @@ uint32_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-    int is_batch_mode = init_monitor(argc, argv);
-
-  /* Receive commands from user. */
-	ui_mainloop(is_batch_mode);
-//	bool success = true;
-//	char str[65536] = "(2)";
-//	uint32_t x = expr(str, &success);
-//	printf("%d", x);
+//    int is_batch_mode = init_monitor(argc, argv);
+//
+//  /* Receive commands from user. */
+//	ui_mainloop(is_batch_mode);
+	bool success = true;
+	char str[65536] = "(2)";
+	uint32_t x = expr(str, &success);
+	printf("%d", x);
 //	FILE *fp = fopen("../tools/gen-expr/input", "r");
 //	assert(fp != NULL);
 //	unsigned int result = 0;
