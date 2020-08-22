a9b7d1b89569c13d004c8ec4e358076f60943bef
tracer-ics2019 <tracer@njuics.org>
2020-08-22 09:52:38 +0800

    >  compile 0905170211 mff Linux mff-Inspiron-5567 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:52:38 up 35 min,  4 users,  load average: 0.28, 0.30, 0.36 ed4ff2aa9f3e20908487df81c05ad55b9c6f6bae

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -106,6 +106,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 }
 
 void difftest_step(vaddr_t ori_pc, vaddr_t next_pc) {
+	printf("get into defftest_step\n");
   CPU_state ref_r;
 
   if (is_detach) return;
