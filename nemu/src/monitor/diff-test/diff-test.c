b1b41dc0180d0cc8df04c961ce5abf0f75af6154
tracer-ics2019 <tracer@njuics.org>
2020-08-22 09:53:45 +0800

    >  compile 0905170211 mff Linux mff-Inspiron-5567 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:53:45 up 36 min,  4 users,  load average: 0.16, 0.26, 0.34 5d84a890de427b37744a4664d424d1c2dcd72e6c

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -138,6 +138,7 @@ void difftest_step(vaddr_t ori_pc, vaddr_t next_pc) {
 
   ref_difftest_exec(1);
   ref_difftest_getregs(&ref_r);
+  printf("get into checkregs\n");
 
   checkregs(&ref_r, ori_pc);
 }
