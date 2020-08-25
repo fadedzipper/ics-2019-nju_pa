489cbf46d752cdb1416297f86c5b35af4ed55573
tracer-ics2019 <tracer@njuics.org>
2020-08-25 22:36:03 +0800

    >  run 0905170211 mff Linux mff-Inspiron-5567 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:36:03 up 13:18,  5 users,  load average: 0.53, 0.36, 0.28 6de5e260cd3f3a8ea3c73983ed30cc76322b1409

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -8,9 +8,11 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
 	  uint32_t in = *(volatile uint32_t *)SCREEN_ADDR;
-	  printf("%08x\n", in);
       info->width = in >> 16;
       info->height = in & 0xff;
+
+	  printf("width = %d, height = %d\n", info->width, info->height);
+
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
