284662487902cf4c693b2abee23814e607e03762
tracer-ics2019 <tracer@njuics.org>
2020-08-25 23:00:12 +0800

    >  run 0905170211 mff Linux mff-Inspiron-5567 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:00:12 up 13:42,  5 users,  load average: 0.58, 0.57, 0.42 2272c2afe1ecb2e4c4fef932b744f2e80ecd186

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -27,8 +27,11 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 		int i, j;
 		for(i = 0; i < ctl->w; i++){
 			for(j = 0; j < ctl->h; j++){
-				printf("reach here\n");
-				outl(FB_ADDR + ctl->x + 400*ctl->y + i + 400*j, *ctl->pixels);
+
+				/* printf("reach here\n"); */
+				/* x, y, i, j */
+
+				outl(FB_ADDR + ctl->y + 400*ctl->x + j + 400*i, *ctl->pixels);
 			}
 		}
 
